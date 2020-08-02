482a58adf3010f5925cdeace3cf4c3655f8fbdbc
giselle2323 <yakubuaminat94@gmail.com>
2020-08-02 23:36:26 +0100

    deploy to netlify

---
+++ build/precache-manifest.b94773552db382dbec23d82279a89266.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7090d16f94a089e5424d",
+    "url": "/static/css/main.57b2a2d0.chunk.css"
+  },
+  {
+    "revision": "7090d16f94a089e5424d",
+    "url": "/static/js/main.7090d16f.chunk.js"
+  },
+  {
+    "revision": "76ae5a18dad89d06ebf1",
+    "url": "/static/js/1.76ae5a18.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a3c5142eb4732fba2b9eae9c439fa12",
+    "url": "/static/media/book.3a3c5142.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "8823e8ac805de00a23427e4ef82e3216",
+    "url": "/index.html"
+  }
+];
